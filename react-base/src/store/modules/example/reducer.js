ca799ebd76ed70406a4e03fd94be56ce8b1a6fe3
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-10-12 12:54:19 -0300

    Redux Saga

--- react-base/src/store/modules/example/reducer.js
+++ react-base/src/store/modules/example/reducer.js
@@ -1,13 +1,26 @@
+import * as types from '../types';
+
 const initialState = {
   clickedButton: false,
 };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case 'CLICKED_BUTTON': {
+    case types.CLICKED_BUTTON_SUCCESS: {
+      console.log('success');
       const newState = { ...state };
       newState.clickedButton = !newState.clickedButton;
       return newState;
+    }
+
+    case types.CLICKED_BUTTON_FAILURE: {
+      console.log('error');
+      return state;
+    }
+
+    case types.CLICKED_BUTTON_REQUEST: {
+      console.log('Doing the request');
+      return state;
     }
 
     default: {
