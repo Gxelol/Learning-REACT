800a037c6aebc98d9047dbb8ffc76760f4af5555
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-10-18 20:48:42 -0300

    useCallback - React Hooks

--- learning-hooks/src/App.js
+++ learning-hooks/src/App.js
@@ -1,95 +1,31 @@
+import P from 'prop-types';
 import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
-const eventFn = () => {
-  console.log('h1 clicked');
-}
+const Button = React.memo(function Button({incrementButton}) {
+  console.log('rendered');
+  return <button onClick={() => incrementButton(10)}>+</button>
+});
+
+Button.propTypes = {
+  incrementButton: P.func,
+};
 
 function App() {
   const [counter, setCounter] = useState(0);
 
-  // componentDidUpdate - runs every time the component updates
-  // useEffect(() => {
-  //   console.log('componentDidUpdate');
-  // });
-  
-  // componentDidMount - run 1x
-  useEffect(() => {
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-
-  // componentWillUmount
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
-  }, []);
-  
-  // With dependency - runs every time the dependency updates
-  // useEffect(() => {
-  //   console.log('Counter changed for', counter);
-  // }, [counter]);
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num);
+  },[]); 
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Counter: {counter}</h1>
-        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <Button incrementButton={incrementCounter} />
       </header>
     </div>
   );
 }
-
-// function App() {
-//   const [reverse, setReverse] = useState(false);
-//   const [counter, setCounter] = useState(0);
-//   const reverseClass = reverse ? 'reverse' : '';
-
-//   const handleClick = () => {
-//     setReverse((reverse) => !reverse)
-//     setCounter((prevCounter) => prevCounter + 1);
-//   }
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-
-//         <h1>Times reversed: {counter}</h1>
-
-//         <button type='button' onClick={handleClick}> Reverse </button>
-//       </header>
-//     </div>
-//   );
-// }
-
-// WITHOUT HOOKS 
-
-//       |
-//       v
-
-// class App extends Component {
-//   state = {
-//     reverse: false,
-//   };
-
-
-//   handleClick = () => {
-//   const { reverse } = this.state;
-//   this.setState({ reverse: !reverse })
-//  }
-
-//   render() {
-//     const { reverse } = this.state;
-//     const reverseClass = reverse ? 'reverse' : '';
-
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-//           <button type='button' onClick={this.handleClick}> Reverse </button>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
