3034ac631c66eab9b31d9db24c64a1eca58ef290
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-10-18 19:11:35 -0300

    useEffect - ReactHooks

--- learning-hooks/src/App.js
+++ learning-hooks/src/App.js
@@ -1,29 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const eventFn = () => {
+  console.log('h1 clicked');
+}
 
 function App() {
-  const [reverse, setReverse] = useState(false);
   const [counter, setCounter] = useState(0);
-  const reverseClass = reverse ? 'reverse' : '';
 
-  const handleClick = () => {
-    setReverse((reverse) => !reverse)
-    setCounter((prevCounter) => prevCounter + 1);
-  }
+  // componentDidUpdate - runs every time the component updates
+  // useEffect(() => {
+  //   console.log('componentDidUpdate');
+  // });
+  
+  // componentDidMount - run 1x
+  useEffect(() => {
+    document.querySelector('h1')?.addEventListener('click', eventFn);
+
+  // componentWillUmount
+    return () => {
+      document.querySelector('h1')?.removeEventListener('click', eventFn);
+    };
+  }, []);
+  
+  // With dependency - runs every time the dependency updates
+  // useEffect(() => {
+  //   console.log('Counter changed for', counter);
+  // }, [counter]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-
-        <h1>Times reversed: {counter}</h1>
-
-        <button type='button' onClick={handleClick}> Reverse </button>
+        <h1>Counter: {counter}</h1>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
       </header>
     </div>
   );
 }
+
+// function App() {
+//   const [reverse, setReverse] = useState(false);
+//   const [counter, setCounter] = useState(0);
+//   const reverseClass = reverse ? 'reverse' : '';
+
+//   const handleClick = () => {
+//     setReverse((reverse) => !reverse)
+//     setCounter((prevCounter) => prevCounter + 1);
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+
+//         <h1>Times reversed: {counter}</h1>
+
+//         <button type='button' onClick={handleClick}> Reverse </button>
+//       </header>
+//     </div>
+//   );
+// }
 
 // WITHOUT HOOKS 
 
