eb0b5cadbb8b1d36f125751d12279e7597274c3f
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-10-19 13:26:58 -0300

    useRef - React Hook

--- learning-hooks/src/App.js
+++ learning-hooks/src/App.js
@@ -1,11 +1,14 @@
 import P from 'prop-types';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 
-const Post = ({post}) => {
+const Post = ({ post, handleClick }) => {
   return (
     <div key={post.id} className="post">
-      <h1>{post.title}</h1>
+      <h1 
+        onClick={() => handleClick(post.title)}>
+        {post.title}
+      </h1>
       <p>{post.body}</p>
     </div>
   )
@@ -16,33 +19,51 @@ Post.propTypes = {
     id: P.number,
     title: P.string,
     body: P.string,
-  })
+  }),
+  handleClick: P.func,
 }
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
+  const input = useRef(null);
+  const counter = useRef(0);
+
   useEffect(() => {
-    setTimeout(() => {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(res => setPosts(res))
-    }, 5000);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(res => setPosts(res))
   }, [])
+
+  useEffect(() => {
+    input.current.focus();
+    console.log(input.current);
+  }, [value])
+
+  useEffect(() => {
+    counter.current++;
+  })
+
+  const handleClick = (value) => {
+    setValue(value)
+  };
 
   return (
     <div className="App">
+      <h1>{counter.current}</h1>
       <p>
-        <input 
-        type="search" 
-        value={value} 
-        onChange={(e) => setValue(e.target.value)} 
+        <input
+          ref={input}
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
         />
       </p>
+
       {useMemo(() => {
         return (
           posts.length > 0 && posts.map(post => {
-            return <Post key={post.id} post={post} />
+            return <Post key={post.id} post={post} handleClick={handleClick}/>
           })
         );
       }, [posts])}
