9e8ef1ad05612a37c963e30f3d6959d0c6fd047b
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-10-19 13:00:32 -0300

    useMemo - React Hooks

--- learning-hooks/src/App.js
+++ learning-hooks/src/App.js
@@ -1,31 +1,55 @@
 import P from 'prop-types';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
-import React, { useState, useEffect, useCallback } from 'react';
 
-const Button = React.memo(function Button({incrementButton}) {
-  console.log('rendered');
-  return <button onClick={() => incrementButton(10)}>+</button>
-});
+const Post = ({post}) => {
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  )
+}
 
-Button.propTypes = {
-  incrementButton: P.func,
-};
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  })
+}
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  const incrementCounter = useCallback((num) => {
-    setCounter((c) => c + num);
-  },[]); 
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(res => setPosts(res))
+    }, 5000);
+  }, [])
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Counter: {counter}</h1>
-        <Button incrementButton={incrementCounter} />
-      </header>
+      <p>
+        <input 
+        type="search" 
+        value={value} 
+        onChange={(e) => setValue(e.target.value)} 
+        />
+      </p>
+      {useMemo(() => {
+        return (
+          posts.length > 0 && posts.map(post => {
+            return <Post key={post.id} post={post} />
+          })
+        );
+      }, [posts])}
+
+      {posts.length <= 0 && <p>Loading...</p>}
     </div>
-  );
+  )
 }
 
 export default App;
