a7d1144680dacbb3839cf20929b7f2d7b8fd5ba2
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-10-19 18:47:44 -0300

    useReducer - React Hook

--- learning-hooks/src/App.js
+++ learning-hooks/src/App.js
@@ -1,12 +1,39 @@
+import { useReducer } from 'react';
 import './App.css';
-import { AppContext } from './contexts/AppContext';
-import { Div } from './components/Div'
+
+export const globalState = {
+  title: 'The title',
+  body: 'The body body saikou',
+  counter: 0,
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'change': {
+      return { ...state, title: action.payload }
+    }
+  }
+  switch (action.type) {
+    case 'reverse': {
+      const { title } = state;
+      return {
+        ...state, title: title.split('').reverse().join('')
+      }
+    }
+  }
+  return { ...state };
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
+
   return (
-  <AppContext>
-    <Div />
-  </AppContext>
+    <div>
+      <h1>{title} {counter}</h1>
+      <button onClick={() => dispatch({ type: 'change', payload: new Date().toLocaleString('pt-BR') })}>Click</button>
+      <button onClick={() => dispatch({ type: 'reverse' })}>Invert</button>
+    </div>
   )
 }
 
