c781c996be2a013f6e4f941d763f034a61ff3104
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-10-19 21:08:22 -0300

    useContext + useReducer

--- learning-hooks/src/App.js
+++ learning-hooks/src/App.js
@@ -1,39 +1,72 @@
-import { useReducer } from 'react';
+import { createContext, useReducer, useContext, useRef } from 'react';
+import P from 'prop-types';
 import './App.css';
 
+// actions.js
+export const actions = {
+  CHANGE_TITLE: 'CHANGE_TITLE'
+}
+
+// data.js
 export const globalState = {
   title: 'The title',
   body: 'The body body saikou',
   counter: 0,
 }
 
-const reducer = (state, action) => {
+// reducer.js
+export const reducer = (state, action) => {
   switch (action.type) {
-    case 'change': {
+    case actions.CHANGE_TITLE: {
+      console.log('Change title')
       return { ...state, title: action.payload }
     }
   }
-  switch (action.type) {
-    case 'reverse': {
-      const { title } = state;
-      return {
-        ...state, title: title.split('').reverse().join('')
-      }
-    }
-  }
-  return { ...state };
+
+  return { ...state }
 }
 
-function App() {
+// AppContext.jsx
+export const Context = createContext();
+export const AppContext = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, globalState);
-  const { counter, title, body } = state;
+
+  const changeTitle = (payload) => {
+    dispatch({ type: actions.CHANGE_TITLE, payload })
+  }
+
+  return <Context.Provider value={{ state, changeTitle }}>{children}</Context.Provider>;
+}
+
+AppContext.propTypes = {
+  children: P.node,
+};
+
+// H1/index.jsx
+export const H1 = () => {
+  const context = useContext(Context);
+  const inputRef = useRef();
 
   return (
-    <div>
-      <h1>{title} {counter}</h1>
-      <button onClick={() => dispatch({ type: 'change', payload: new Date().toLocaleString('pt-BR') })}>Click</button>
-      <button onClick={() => dispatch({ type: 'reverse' })}>Invert</button>
-    </div>
+    <>
+      <h1
+        onClick={() => context.changeTitle(inputRef.current.value)}>
+        {context.state.title}
+      </h1>
+      <input type="text" ref={inputRef}/>
+    </>
+  )
+}
+
+//App.jsx
+function App() {
+
+  return (
+    <AppContext>
+      <div>
+        <H1 />
+      </div>
+    </AppContext>
   )
 }
 
