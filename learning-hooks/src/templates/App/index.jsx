d19cb6dc6ec1374aeda4fb62ab6fdc22f170d9e0
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-10-21 18:47:15 -0300

    useDebugValue and custom useMediaMatch

--- learning-hooks/src/templates/App/index.jsx
+++ learning-hooks/src/templates/App/index.jsx
@@ -1,48 +1,46 @@
-/* eslint-disable react/prop-types */
-import { forwardRef, useImperativeHandle, useLayoutEffect, useRef, useState } from 'react';
+import { useDebugValue, useEffect, useState } from 'react';
 import './styles.css';
 
-export const App = () => {
-  const [count, setCount] = useState([0, 1 , 2, 3, 4]);
-  const divRef = useRef();
+const useMediaMatch = (queryValue, initialValue = false) => {
+  const [match, setMatch] = useState(initialValue);
 
-  useLayoutEffect(() => {
-    const now = Date.now();
-    while (Date.now() < now + 300);
-    divRef.current.divRef.scrollTop = divRef.current.divRef.scrollHeight;
-  });
+  useDebugValue(`Query: ${queryValue}`, name => {
+    return name + 'modified';
+  })
 
-  const handleClick = () => {
-    setCount((c) => [...c, +c.slice(-1) + 1])
-    divRef.current.handleClick();
-  };
-  
-  return (
-    <>
-    <button onClick={ handleClick }>Count { count.slice(-1) }</button>
-    <DisplayCount count={count} ref={ divRef } />
-    </>
-  )
+  useEffect(() => {
+    let isMounted = true;
+    const matchMedia = window.matchMedia(queryValue);
+
+    const handleChange = () => {
+      if (!isMounted) return;
+      setMatch(!!matchMedia.matches);
+    }
+
+    matchMedia.addEventListener('change', handleChange);
+    setMatch(!!matchMedia.matches);
+
+    return () => {
+      isMounted = false;
+      matchMedia.removeEventListener('change', handleChange);
+    }
+  }, [queryValue])
+
+  return match;
 };
 
-export const DisplayCount = forwardRef(function DisplayCount({ count }, ref) {
-  const [rand, setRand] = useState('0.32')
-  const divRef = useRef();
-
-  const handleClick = () => {
-    setRand(Math.random().toFixed(2));
-  };
-
-  useImperativeHandle(ref, () => ({
-    handleClick,
-    divRef: divRef.current,
-  }));
+export const App = () => {
+  const enormous = useMediaMatch('(min-width: 1280px)');
+  const huge = useMediaMatch('(max-width: 1279px) and (min-width: 980px)');
+  const big = useMediaMatch('(max-width: 979px) and (min-width: 768px)');
+  const medium = useMediaMatch('(max-width: 767px) and (min-width: 321px)');
+  const small = useMediaMatch('(max-width: 320px)');
+  const background = enormous? 'darkblue' : huge? 'yellow' : big? 'red' : medium? 'pink' : small? 'cyan' : null
+  const color = enormous? 'white' : huge? 'white' : big? 'white' : medium? 'white' : small? 'white' : null
 
   return (
-    <div ref={divRef} style={{height: '200px', width: '200px', overflowY: 'scroll'}}>
-      {count.map((c) =>{
-        return <p onClick={handleClick} key={c}>{c} {rand}</p>
-      })};
+    <div style={{fontSize: '50px', background, color}}>
+      Hello World!
     </div>
   )
-});
+};
