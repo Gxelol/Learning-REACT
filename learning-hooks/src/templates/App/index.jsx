0561b17ac586067572b56ad7e63dfa286909a666
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-10-20 22:43:02 -0300

    custom hook async

--- learning-hooks/src/templates/App/index.jsx
+++ learning-hooks/src/templates/App/index.jsx
@@ -1,107 +1,73 @@
-import P from 'prop-types'
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './styles.css';
 
-const isObjectEqual = (objA, objB) => {
-  return JSON.stringify(objA) === JSON.stringify(objB)
-};
-
-const useFetch = (url, options) => {
-  const [resultState, setResult] = useState(null);
-  const [loadingState, setLoading] = useState(false);
-  const [shouldLoud, setShouldLoad] = useState(false);
-  const urlRef = useRef(url);
-  const optionsRef = useRef(options);
-
-  useEffect(() => {
-    let changed = false;
-
-    if (!isObjectEqual(options, optionsRef.current)) {
-      optionsRef.current = options;
-      changed = true;
-    }
-    if (!isObjectEqual(url, urlRef.current)) {
-      urlRef.current = url;
-      changed = true;
-    }
-
-    if (changed) {
-      setShouldLoad((s) => !s);
-    }
-
-  }, [url, options])
-
-  useEffect(() => {
-    let wait = false;
-    
-    const abortController = new AbortController();
-    const signal = abortController.signal;
-
-    setLoading(true);
-
-    const fetchData = async () => {
-      await new Promise(r => setTimeout(r, 1000))
-      try {
-        const response = await fetch(urlRef.current, { signal, ...optionsRef.current });
-        const jsonResult = await response.json();
-        if (!wait) {
-          setResult(jsonResult);
-          setLoading(false);
-        }
-      } catch (e) {
-        if (!wait) {
-          setLoading(false);
-          console.warn();
-        }
-      }
-    }
-
-    fetchData();
-
-    return () => {
-      wait = true;
-      abortController.abort();
-    }
-  }, [shouldLoud])
-
-  return [resultState, loadingState];
-}
-
-export const App = () => {
-  const [postId, setPostId] = useState('');
-  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId, {
-    headers: {
-      abc: '1' + postId,
-    }
+const useAsync = (asyncFunction, shouldRun) => {
+  const [state, setState] = useState({
+    result: null, 
+    error: null,
+    status: 'idle',
   });
 
-  if (loading) {
-    return <p>Loading...</p>
+  const run = useCallback(() => {
+    console.log('EFFECT', new Date().toLocaleString());
+    
+    setState({
+    result: null, 
+    error: null,
+    status: 'pending'   
+    });
+
+    return asyncFunction()
+      .then((response) => {
+        setState({
+          result: response, 
+          error: null,
+          status: 'settled',   
+          });
+      })
+      .catch((error) => {
+        setState({
+          result: null, 
+          error: error,
+          status: 'error',   
+          });
+      })
+  }, [asyncFunction])
+
+  useEffect(() => {
+    if (shouldRun) {
+      run();
+    }
+  }, [run, shouldRun])
+
+  return [run, state.result, state.error, state.status];
+};
+
+const fetchData = async () => {
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts/');
+  const json = await data.json();
+  return json;
+};
+
+export const App = () => {
+  const [posts, setPosts] = useState(null);
+  const [reFetchData, result, error, status] = useAsync(fetchData, true);
+
+  if (status === 'idle') {
+    return <pre>Nothing is running</pre>
   }
 
-  const handleClick = async (id) => {
-    setPostId(id);
+  if (status === 'pending') {
+    return <pre>Loading...</pre>
   }
 
-  if (!loading && result) {
-    return (
-      <div>
-        {result?.length > 0 ? result.map((p) =>
-        <div key={p.id} onClick={() => handleClick(p.id)}>
-          <p>{p.title}</p>
-        </div>
-        ) : (
-          <div onClick={() => handleClick('')}>
-            <p>{result.title}</p>
-          </div>
-        )}
-      </div>
-    )
+  if (status === 'settled') {
+    return <pre>{JSON.stringify(result, null, 2)}</pre>
   }
 
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  )
+  if (status === 'error') {
+    return <pre>{JSON.stringify(error, null, 2)}</pre>
+  }
+
+  return 'OOOOOO';
 }
