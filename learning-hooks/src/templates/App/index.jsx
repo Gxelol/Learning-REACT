cce7bc14d3b9f79d919161ded3d901b7b69e23dc
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-10-21 14:24:29 -0300

    useLayoutEffect - React Hook

--- learning-hooks/src/templates/App/index.jsx
+++ learning-hooks/src/templates/App/index.jsx
@@ -1,73 +1,27 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
 import './styles.css';
 
-const useAsync = (asyncFunction, shouldRun) => {
-  const [state, setState] = useState({
-    result: null, 
-    error: null,
-    status: 'idle',
+export const App = () => {
+  const [count, setCount] = useState([0, 1 , 2, 3, 4]);
+  const divRef = useRef();
+
+  useLayoutEffect(() => {
+    divRef.current.scrollTop = divRef.current.scrollHeight;
   });
 
-  const run = useCallback(() => {
-    console.log('EFFECT', new Date().toLocaleString());
-    
-    setState({
-    result: null, 
-    error: null,
-    status: 'pending'   
-    });
-
-    return asyncFunction()
-      .then((response) => {
-        setState({
-          result: response, 
-          error: null,
-          status: 'settled',   
-          });
-      })
-      .catch((error) => {
-        setState({
-          result: null, 
-          error: error,
-          status: 'error',   
-          });
-      })
-  }, [asyncFunction])
-
-  useEffect(() => {
-    if (shouldRun) {
-      run();
-    }
-  }, [run, shouldRun])
-
-  return [run, state.result, state.error, state.status];
+  const handleClick = () => {
+    setCount((c) => [...c, +c.slice(-1) + 1])
+  };
+  
+  return (
+    <>
+    <p>Hello World!</p>
+    <button onClick={handleClick}>Count {count.slice(-1)}</button>
+    <div ref={divRef} style={{height: '200px', width: '200px', overflowY: 'scroll'}}>
+      {count.map((c) =>{
+        return <p key={c}>{c}</p>
+      })};
+    </div>
+    </>
+  )
 };
-
-const fetchData = async () => {
-  const data = await fetch('https://jsonplaceholder.typicode.com/posts/');
-  const json = await data.json();
-  return json;
-};
-
-export const App = () => {
-  const [posts, setPosts] = useState(null);
-  const [reFetchData, result, error, status] = useAsync(fetchData, true);
-
-  if (status === 'idle') {
-    return <pre>Nothing is running</pre>
-  }
-
-  if (status === 'pending') {
-    return <pre>Loading...</pre>
-  }
-
-  if (status === 'settled') {
-    return <pre>{JSON.stringify(result, null, 2)}</pre>
-  }
-
-  if (status === 'error') {
-    return <pre>{JSON.stringify(error, null, 2)}</pre>
-  }
-
-  return 'OOOOOO';
-}
