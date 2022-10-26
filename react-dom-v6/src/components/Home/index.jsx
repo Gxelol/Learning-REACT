c199a78d6e3651cef85f23ab81c953e303aef926
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-10-25 22:35:05 -0300

    BrowserRouter, Routes, Route and Link

--- react-dom-v6/src/components/Home/index.jsx
+++ react-dom-v6/src/components/Home/index.jsx
@@ -1,7 +1,7 @@
 export const Home = () => {
   return (
     <div>
-      <h1>Hello WORLD!</h1>
+      <h1>Hello World!</h1>
     </div>
   );
 };
--- react-dom-v6/src/main.jsx
+++ react-dom-v6/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './components/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './styles/global.css'
+import { About } from './components/About'
+import { Menu } from './components/Menu'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
