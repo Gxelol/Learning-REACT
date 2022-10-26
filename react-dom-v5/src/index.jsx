ff3118e7534b2232524d64fa511c26c25de9ca9e
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-10-25 21:06:50 -0300

    BrowserRouter, Switch, Route and Menu

---
+++ react-dom-v5/src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Menu } from './components/Menu';
+import './index.css';
+import { Abc } from './templates/Abc';
+import { App } from './templates/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Menu />
+      <Switch>
+        <Route path="/" component={App} exact />
+        <Route path="/abc/:slug?/:id?" component={Abc} exact />
+      </Switch>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
